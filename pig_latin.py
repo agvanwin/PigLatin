69e34ba512bf7b7eaca8327da4acd492e50db791
agvanwin <aaron.vanwinkle13@gmail.com>
2015-05-18 22:22:58 -0400

    Update pig_latin.py

--- pig_latin.py
+++ pig_latin.py
@@ -5,7 +5,7 @@ original = raw_input('Enter a word:')
 if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = original[0]
-    new_word = word[1:4] + first + pyg
+    new_word = word[1:] + first + pyg
     print new_word
 
 else:
